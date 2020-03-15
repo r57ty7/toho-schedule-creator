01edca3731e10308306277344f8fe478110da097
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-03-15 23:05:26 +0900

    add mocking test

--- src/email.test.ts
+++ src/email.test.ts
@@ -1,6 +1,49 @@
 import { searchTohoEmail, createMovieFromEmailBody } from './Email'
 import Movie from './movie'
 
+class GmailThread {
+  message: GmailMessage
+  constructor(message: string) {
+    this.message = new GmailMessage(message)
+  }
+
+  getMessages(): GmailMessage[] {
+    const ms: GmailMessage[] = []
+    ms.push(this.message)
+    return ms
+  }
+}
+class GmailMessage {
+  message: string
+  constructor(message: string) {
+    this.message = message
+  }
+}
+
+describe('searchTohoEmail', (): void => {
+  test('normal', (): void => {
+    GmailApp.search = jest.fn((arg: string): any[] => {
+      const ts: GmailThread[] = []
+      if ('from:i-net.ticket@ml.tohotheater.jp subject:"Notice of Internet Ticket Purchase Completion" after:2020-01-02') {
+        const t = new GmailThread('my mail')
+        ts.push(t)
+        return ts
+      }
+      return ts
+    })
+
+    const day = new Date('2020-01-02T15:04:05+09:00')
+    const actual = searchTohoEmail(day)
+    const expected: GmailMessage[] = [
+      {
+        message: 'my mail',
+      },
+    ]
+    expect(GmailApp.search).toBeCalledTimes(1)
+    expect(actual).toEqual(expected)
+  })
+})
+
 describe('createMovieFromEmailBody', (): void => {
   test('normal', (): void => {
     const arg = `
